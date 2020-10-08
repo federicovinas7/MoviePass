5f10726229d1e2bacdbe94d996bb0ec27bf5cc1d
Nahuel Flores <66324788+Nahuel-Flores@users.noreply.github.com>
2020-10-08 13:13:40 -0300

    Revisar contenedores porque solo funcion "container"

--- Views/movies_list.php
+++ Views/movies_list.php
@@ -2,25 +2,22 @@
     require_once("header.php");
 ?>
 
-<main class="container-movies">
+<main class="container">
     <h1>Movies</h1>
     <div class="custom-scrollbar table-wrapper-scroll-y">    
-    <div class="row">
-            
-            <?php foreach ($cinemas as $key => $value) { ?>
-                <div class="col-md-3">    
-            
-                    <div class="card" style="width: 18rem;">
-                        <img class="card-img-top rounded" src="<?php echo $value->getPoster() ?>" alt="Card image cap" >
-                        <div class="card-body">
-                            <h5 class="card-title"><strong><?php echo $value->getTitle() ?></strong></h5>
+        <div class="row">
+                <?php foreach ($cinemas as $key => $value) { ?>
+                    <div class="col-md-4">    
+                
+                        <div class="card" style="width: 18rem;">
+                            <img class="card-img-top rounded" src="<?php echo $value->getPoster() ?>" alt="Card image cap" >
+                            <div class="card-body">
+                                <h5 class="card-title"><strong><?php echo $value->getTitle() ?></strong></h5>
+                            </div>
                         </div>
+                        <br>
                     </div>
-                    <br>
-                </div>
-            <?php } ?>
-            
-            
+                <?php } ?>
         </div>
-
+    </div>           
 </main>
