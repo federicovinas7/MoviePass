042ed15f8db8337caeed8767d4e82fc2975b77f0
Nahuel Flores <66324788+Nahuel-Flores@users.noreply.github.com>
2020-10-10 18:41:14 -0300

    Primera implementacion de modal falta solucionar problemas de coneccion entre boton y modal

--- Views/movies_list.php
+++ Views/movies_list.php
@@ -2,22 +2,38 @@
     require_once("header.php");
 ?>
 
-<main class="container">
+<main class="container_movies">
     <h1>Movies</h1>
     <div class="custom-scrollbar table-wrapper-scroll-y">    
         <div class="row">
-                <?php foreach ($cinemas as $key => $value) { ?>
-                    <div class="col-md-4">    
-                
-                        <div class="card" style="width: 18rem;">
-                            <img class="card-img-top rounded" src="<?php echo $value->getPoster() ?>" alt="Card image cap" >
-                            <div class="card-body">
-                                <h5 class="card-title"><strong><?php echo $value->getTitle() ?></strong></h5>
+            <?php foreach ($cinemas as $key => $value) { ?>
+                <div class="col-md-3">    
+                    <button type="button" class="btn btn-dark"  data-id="" data-toggle="modal" data-target=".bd-example-modal-lg"> 
+                        
+                        <figure class="figure">
+                            <img class="figure-img img-fluid rounded" src="<?php echo $value->getPoster() ?>" width="60%" >
+                            <figcaption class="figure-caption"><?php echo $value->getTitle() ?></figcaption>
+                        </figure>
+                        
+                    </button>
+
+                    <div class="modal fade bd-example-modal-lg"  id="" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-lg">
+                            <div class="modal-content">
+                                <div class="media">
+                                    <img class="align-self-center mr-3" src="<?php echo $value->getPoster() ?>" width="70%">
+                                    <div class="media-body">
+                                        <h5 class="mt-0"><?php echo $value->getTitle() ?></h5>
+                                        
+                                        <p> <?php echo $value->getSynopsis()  ?> </p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <br>
-                    </div>
-                <?php } ?>
+                    </div>          
+                </div>
+                <br>
+            <?php } ?>
         </div>
     </div>           
 </main>
