8e73d8e1d5c2503f92931939993c741afba1cfe8
Nahuel Flores <66324788+Nahuel-Flores@users.noreply.github.com>
2020-10-11 20:52:30 -0300

    Se corriguieron errores en los modals todavia falta solucionar lo que muestra

--- Views/movies_list.php
+++ Views/movies_list.php
@@ -8,7 +8,7 @@
         <div class="row">
             <?php foreach ($cinemas as $key => $value) { ?>
                 <div class="col-md-3">    
-                    <button type="button" class="btn btn-dark"  data-id="" data-toggle="modal" data-target=".bd-example-modal-lg"> 
+                    <button type="button" class="btn btn-dark"  data-id="<?php echo $value->getId() ?>" data-toggle="modal" data-target=".movie"> 
                         
                         <figure class="figure">
                             <img class="figure-img img-fluid rounded" src="<?php echo $value->getPoster() ?>" width="60%" >
@@ -16,24 +16,26 @@
                         </figure>
                         
                     </button>
-
-                    <div class="modal fade bd-example-modal-lg"  id="" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-                        <div class="modal-dialog modal-lg">
-                            <div class="modal-content">
-                                <div class="media">
-                                    <img class="align-self-center mr-3" src="<?php echo $value->getPoster() ?>" width="70%">
-                                    <div class="media-body">
-                                        <h5 class="mt-0"><?php echo $value->getTitle() ?></h5>
-                                        
-                                        <p> <?php echo $value->getSynopsis()  ?> </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>          
                 </div>
                 <br>
             <?php } ?>
+
+            <div class="modal fade movie"  id="" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-lg">
+                    <div class="modal-content">
+                        <div class="media">
+                            <img class="align-self-center mr-3" src="<?php echo $value->getPoster() ?>" width="70%">
+                            <div class="media-body">
+                                <h5 class="mt-0"><?php echo $value->getTitle() ?></h5>
+                                        
+                                <p> <?php echo $value->getSynopsis() ?> </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>          
+
+
         </div>
     </div>           
 </main>
