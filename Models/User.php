bfee2e92576916be6a0cdd4179bef1d011b18a31
Nahuel Flores <66324788+Nahuel-Flores@users.noreply.github.com>
2020-10-23 12:32:50 -0300

    Usuario models

--- Models/User.php
+++ Models/User.php
@@ -8,15 +8,70 @@
         private $firstName;
         private $lastName;
         private $birthdate;
+        private $userType;
 
 
-        public function __construct($email,$password,$firstName,$lastName,$birthdate){
+        public function __construct($email,$password,$firstName,$lastName,$birthdate,$userType){
             $this->email = $email;
             $this->password = $password;
             $this->firstName = $firstName;
             $this->lastName = $lastName;
             $this->birthdate = $birthdate;
+            $this->userType = $userType;
         }
 
+
+        //-----------------Getters-----------------
+
+        public function getEmail(){
+            return $this->email;
+        }
+
+        public function getPassword(){
+            return $this->password;
+        }
+
+        public function getFirstName(){
+            return $this->firstName;
+        }
+
+        public function getLastName(){
+            return $this->lastName;
+        }
+
+        public function getBirthdate(){
+            return $this->birthdate;
+        }
+
+        public function getUserType(){
+            return $this->userType;
+        }
+
+        //-----------------Setters-----------------
+        
+        public function setEmail($email){
+            return $this->email = $email;
+        }
+
+        public function setPassword($password){
+            return $this->password = $password;
+        }
+
+        public function setFirstName($firstName){
+            return $this->firstName = $firstName;
+        }
+
+        public function setLastName($lastName){
+            return $this->lastName = $lastName;
+        }
+
+        public function getBirthdate($birthdate){
+            return $this->birthdate = $birthdate;
+        }
+
+        public function setUserType($userType){
+            return $this->userType = $userType;
+        }
+    
     }
 ?>
